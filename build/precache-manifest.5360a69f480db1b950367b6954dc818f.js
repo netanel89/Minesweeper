a53ecdda30aff0cb40023fff2a87e951e4b67750
Netanel Abekasis <37871544+netanel89@users.noreply.github.com>
2019-12-25 16:48:18 +0200

    upload build folder for runnable index

---
+++ build/precache-manifest.5360a69f480db1b950367b6954dc818f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce26b7cb34e943572e625dd55c920fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b09d6ebb93279933dcc",
+    "url": "/static/js/2.cc6b6e05.chunk.js"
+  },
+  {
+    "revision": "0027fda88cd1d9dfe2f0",
+    "url": "/static/js/main.d9a3a1ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
